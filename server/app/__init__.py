8cbfcbbe0dfe2c8bf25291d4a6c683fe455df50c
thenry3 <43651306+thenry3@users.noreply.github.com>
2019-11-17 14:58:40 -0800

    sdf

---
+++ server/app/__init__.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template, make_response
+
+# app = Flask(__name__, static_folder="../../client/build/static",
+#             template_folder="../../client/build")
+app = Flask(__name__, static_folder="../../client/build/static",
+            template_folder="../../client/build")
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
