e4b1e81516fc66d877550b85f1b935e8771e3897
thenry3 <43651306+thenry3@users.noreply.github.com>
2019-11-17 14:41:15 -0800

    asd

--- server/flask_app/__init__.py
+++ server/flask_app/__init__.py
@@ -9,8 +9,5 @@ def index():
     return render_template('index.html')
 
 
-app.run()
-
-
 # if __name__ == '__main__':
 #     app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
