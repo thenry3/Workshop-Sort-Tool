e85567219f7d56c15c4e246a49f124248672fdb2
thenry3 <43651306+thenry3@users.noreply.github.com>
2019-11-17 14:12:40 -0800

    initialize python

---
+++ server/src/app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, make_response
+
+app = Flask(__name__, static_folder="../../client/build/static",
+            template_folder="../../client/build")
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+# if __name__ == '__main__':
+#     app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
