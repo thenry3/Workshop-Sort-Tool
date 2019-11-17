66f7c84bc0d0e5ec0e00184188a9fd2fda16f846
thenry3 <43651306+thenry3@users.noreply.github.com>
2019-11-17 14:22:54 -0800

    small

--- server/src/app.py
+++ server/src/app.py
@@ -9,5 +9,8 @@ def index():
     return render_template('index.html')
 
 
+app.run()
+
+
 # if __name__ == '__main__':
 #     app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
